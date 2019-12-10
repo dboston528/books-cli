c6d0e2f6e8b9e24545d16fc7e781bfd536f18448
DeAndre Boston <dboston3@yahoo.com>
2019-12-09 23:29:11 -0600

    Update try catch

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-const axios = require("axios");
-const mongoose = require("mongoose");
+const axios = require('axios');
+const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const db = mongoose.connect("mongodb://localhost:27017/bookcli", {
+const db = mongoose.connect('mongodb://localhost:27017/bookcli', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
 //seaech for book using google api
 searchBook = input => {
-  const url = "https://www.googleapis.com/books/v1/volumes?q=";
+  const url = 'https://www.googleapis.com/books/v1/volumes?q=';
   axios
     .get(
       url +
         input.bookname +
-        "&maxResults=5" +
-        "&fields=items(volumeInfo/title, volumeInfo/authors, volumeInfo/publisher)"
+        '&maxResults=5' +
+        '&fields=items(volumeInfo/title, volumeInfo/authors, volumeInfo/publisher)'
     )
     .then(res => {
-      const response = res.data.items;
-      const books = response.map(book => book.volumeInfo);
-      console.info(books);
-      mongoose.connection.close();
+      try {
+        const response = res.data.items;
+        const books = response.map(book => book.volumeInfo);
+        console.info(books);
+        mongoose.connection.close();
+      } catch (error) {
+        console.info(
+          'Houston we have a problem. That query did not return any results.'
+        );
+        mongoose.connection.close();
+      }
     });
 };
 
 //add book to my data base
 const addBook = () => {
   Book.create(customer).then(book => {
-    ("You have added a book!");
+    ('You have added a book!');
     mongoose.connection.close();
   });
 };
